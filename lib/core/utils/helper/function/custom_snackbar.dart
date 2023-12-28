4c82385029a7fff12448e11c6391e9b84d814692
ahmed ragab <ar1755@fayoum.edu.eg>
2023-12-28 02:32:52 +0200

    change the color of snack bar

--- lib/core/utils/helper/function/custom_snackbar.dart
+++ lib/core/utils/helper/function/custom_snackbar.dart
@@ -1,9 +1,11 @@
+import 'package:bookly/core/utils/colors/app_color.dart';
 import 'package:flutter/material.dart';
 
 void customSnackBar(context, String text) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: Text(text),
+      backgroundColor: Colors.black,
+      content: Text(text,style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: whiteColor),),
     ),
   );
 }
